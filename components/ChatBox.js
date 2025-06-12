74001f04ed8f7dbf458862b995e2ef327724d708
Trent2000Steel <thomas4out@gmail.com>
2025-06-12 14:17:21 -0500

    Add files via upload

--- components/ChatBox.js
+++ components/ChatBox.js
@@ -1,7 +1,32 @@
-import useChatFlow from './ChatFlow';
-import ChatUI from './ChatUI';
 
-export default function ChatBox() {
-  const flow = useChatFlow();
-  return <ChatUI {...flow} />;
-}
+import React, { useState } from "react";
+import ChatUI from "./ChatUI";
+import ChatOpener from "./ChatOpener";
+import EstimateFlow from "./EstimateFlow";
+import ChatFlow_Closing from "./ChatFlow_Closing";
+
+const ChatBox = () => {
+  const [stage, setStage] = useState("opener");
+  const [moveDetails, setMoveDetails] = useState(null);
+
+  const handleOpenerComplete = (details) => {
+    setMoveDetails(details);
+    setStage("estimate");
+  };
+
+  const handleEstimateComplete = () => {
+    setStage("closing");
+  };
+
+  return (
+    <ChatUI>
+      {stage === "opener" && <ChatOpener onComplete={handleOpenerComplete} />}
+      {stage === "estimate" && (
+        <EstimateFlow moveDetails={moveDetails} onComplete={handleEstimateComplete} />
+      )}
+      {stage === "closing" && <ChatFlow_Closing moveDetails={moveDetails} />}
+    </ChatUI>
+  );
+};
+
+export default ChatBox;
